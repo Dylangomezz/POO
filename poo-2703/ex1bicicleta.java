ff09ce65f91cdac47ee82e5b9fc2d64046307a5b
Dylangomezz <dylangomesf@gmail.com>
2026-03-28 17:06:27 -0300

    a

--- poo-2703/ex1bicicleta.java
+++ poo-2703/ex1bicicleta.java
@@ -1,16 +1,85 @@
+/******************************************************************************
 
+27032026 - Classe Bicicleta (Codigo base)
+
+Implemente uma classe Bicicleta para representar uma bicicleta. 
+Toda bicicleta tem uma velocidade (atributo velocidade) e uma marcha (atributo marcha). 
+Além disto, a bicicleta pode: acelerar (método acelerar), frear (método frear) e 
+apresentar o seu estado atual (método imprimirEstados).
+
+Em sua construção a classe deve iniciar na marcha 0 (zero) e com velocidade 0 (zero). 
+ 
+Implemente uma segunda classe (Classe Main) como sendo a classe principal do sistema. 
+Ela deve ter o método main e ser responsável por criar objetos do tipo bicicletas, interagir com seus 
+métodos e, a cada interação, imprimir da saída padrão o estado atual a bicicleta 
+manipulada. 
+
+Implementação dos Métodos;
+Altere a classe do exercício anterior para que ela apresente um novo atributo que 
+representa a velocidade máxima desempenhada pela bicicleta. De forma específica: 
+ 
+a) Inclua o atributo velocidadeMaxima, que representa o valor máximo que o 
+atributo velocidade pode apresentar. 
+              
+b) O valor de velocidadeMaxima deve ser atribuído no momento em que o 
+objeto é criado utilizando um construtor de inicialização. A classe Bicicleta não deverá 
+mais ter construtor padrão. 
+      
+c) O método imprimirEstados, quando invocado, deve apresentar (na saída 
+padrão) o valor da velocidadeMaxima que foi atribuída ao objeto, além dos valores dos 
+outros atributos. 
+ 
+ d) Ao incrementar a velocidade, o objeto deve impedir que a velocidade da 
+bicicleta supere a velocidadeMaxima.
+
+No main, crie 3 bicicletas e execute as seguintes operações:
+    
+    Objeto "bicicletaUrbana": 
+    *Possui velocidade máxima de 40.
+    *Acelere em 15
+    *Mude para a Marcha 2
+    *Imprima o estado
+
+    Objeto "bicicletaCorrida":
+    *Possui velocidade máxima de 80
+    *Acelere em 50
+    *Mude para a Marcha 5
+    *Imprima o estado
+    *Acelere em 40 (tentativa de ultrapassar a velocidade máxima)
+    *Imprima o estado
+
+    Objeto "bicicletaInfantil":
+    *Possui velocidade máxima de 15
+    *Acelere em 10
+    *Freie em 5
+    *Mude para a Marcha 1
+    *Imprima o estado
+    *Freie em 20 (tentativa de frear além da velocidade atual)
+    *Imprima o estado
+
+
+*******************************************************************************/
 class Bicicleta{
-    private double velocidade;
-    private double marcha;
-
-    public Bicicleta(){}
-
-    public double getVelocidade(){
-        return this.velocidade;
+    private int velocidade;
+    private int marcha;
+    private int velocidadeMaxima;
+    //construtor
+    public Bicicleta(int velocidadeMaxima){
+        this.velocidade = 0;
+        this.marcha = 0;
+        this.velocidadeMaxima = velocidadeMaxima;
     }
-    public void setVelocidade(){
-        
+    //metodo acelerar
+    public void acelerar(int incremento){
+        if (velocidade + incremento > velocidadeMaxima) {
+            velocidade = velocidadeMaxima;
+            System.out.println("Atingiu a velocidade máxima!");
+        } else {
+            velocidade += incremento;
+        }
     }
+
+
 }
 
 public class ex1bicicleta{}
